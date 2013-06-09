252573fbcb41d962dcc950a12c2be01a7f0c55f4
Bruno Barros <bkether@gmail.com>
2013-06-09 01:35:35 -0300

    Aws Example First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwsExample::Application.config.secret_key_base = 'c7907de3703b029a11e7810ab239fbd1823cd736e7524f942cf66f5154f8aac7696eb1d07444cb46660ad49a26bd062d577a854c0b05c910bfde729d2f0fb306'
